9281916ba5e0f53e2ec0bb3d0939777742aa30bd
nsavva38 <nsavva38@gmail.com>
2024-10-29 14:08:54 -0400

    Picking Colors functional

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,25 @@
+import { useState } from 'react'
+
+
 // Write your Color component here
-const Color = ({ color }) => {
-  return <div className={color}></div>;
+const Color = ({ color, setSelectedColor }) => {
+  return <div className={color} onClick={() => setSelectedColor(color)}></div>;
 };
 
 const App = () => {
+
+  const [selectedColor, setSelectedColor] = useState("");
+
   return (
     <div id="container">
       <div id="navbar">
         <div>Currently selected: </div>
-        <div className="red">red</div>
+        <div className={selectedColor}>{selectedColor}</div>
       </div>
       <div id="colors-list">
-        <Color color="black"/>
-        <Color color="violet"/>
-        <Color color="orange"/>  
+        <Color color="black" setSelectedColor={setSelectedColor}/>
+        <Color color="violet" setSelectedColor={setSelectedColor}/>
+        <Color color="orange" setSelectedColor={setSelectedColor}/>  
       </div>
     </div>
   );
